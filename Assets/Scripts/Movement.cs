e18be234f8fe9f43749dcb24f8310accf7707fd2
Kevin Sinclair <kevinsinclair190@gmail.com>
2025-12-29 18:38:22 +0530

    Rocket Flies
    Rocket Flies up when space bar is pressed

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+   [SerializeField] InputAction thrust;
+   [SerializeField] float thrustStrength = 100f;
+
+   Rigidbody rb;
+
+   private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    private void Update()
+    {
+        if(thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+}
